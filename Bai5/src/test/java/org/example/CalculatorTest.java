202082143da26c99bcc616243a15eec4bd651508
25023260-dot <25023260@vnu.edu.vn>
2026-05-14 22:07:14 +0700

    Add separate workflows for Bai4 and Bai5

---
+++ Bai5/src/test/java/org/example/CalculatorTest.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CalculatorTest {
+
+    @Test
+    void testAdd() {
+        Calculator c = new Calculator();
+        assertEquals(5, c.add(2, 3));
+    }
+
+    @Test
+    void testSubtract() {
+        Calculator c = new Calculator();
+        assertEquals(1, c.subtract(3, 2));
+    }
+
+    @Test
+    void testMultiply() {
+        Calculator c = new Calculator();
+        assertEquals(6, c.multiply(2, 3));
+    }
+}
